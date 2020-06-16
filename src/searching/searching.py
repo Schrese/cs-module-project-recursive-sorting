706810e2a4966d0e83a9c4be6a730efbfb240cc3
Schrese <schreseholloway@gmail.com>
2020-06-16 16:10:26 -0400

    planning out the binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # Base Case: when the target is found OR when the high/low pointers are at the same index
+    # How to get there: increment/decrement depending on the input
+    # Conditionals:
+    # if the target is found
+        # return that index
+    # if the target is less than my guess
+        # I need to re-run the method while changing start to be the guess - 1 and the end would stay the same
+    # if the target is more than my guess
+        # I need to re-run the method while changing end to be the guess + 1 and the start would stay the same
+    # if the start and end are the same, then that means the target isn't in the list
+        # so i just return -1
 
 
 # STRETCH: implement an order-agnostic binary search
