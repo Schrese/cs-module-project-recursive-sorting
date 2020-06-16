242b79c249e3e2e192ab3c9ffb5fff8a4f37bf6b
Schrese <schreseholloway@gmail.com>
2020-06-16 17:03:27 -0400

    Seems I was super close. Forgot that I still need the while loop...

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -4,14 +4,26 @@ def binary_search(arr, target, start, end):
     # Base Case: when the target is found OR when the high/low pointers are at the same index
     # How to get there: increment/decrement depending on the input
     # Conditionals:
-    # if the target is found
-        # return that index
-    # if the target is less than my guess
-        # I need to re-run the method while changing start to be the guess - 1 and the end would stay the same
-    # if the target is more than my guess
-        # I need to re-run the method while changing end to be the guess + 1 and the start would stay the same
-    # if the start and end are the same, then that means the target isn't in the list
-        # so i just return -1
+    # print('hello')
+    mid = (start + end) // 2
+    while start <= end:
+        # if the target is found
+        if arr[mid] == target:
+            # return that index
+            return mid
+        # if the target is less than my guess
+        if target < arr[mid]:
+            # I need to re-run the method while changing start to be the guess - 1 and the end would stay the same
+            return binary_search(arr, target, start, mid - 1)
+        # if the target is more than my guess
+        if target > arr[mid]:
+            # I need to re-run the method while changing end to be the guess + 1 and the start would stay the same
+            return binary_search(arr, target, mid + 1, end)
+        # if the start and end are the same, then that means the target isn't in the list
+            # so i just return -1
+    return -1
+
+# I don't quite know how to have planned the while loop. I planned out literally everything else (with the exception of getting the +1 and -1 backwards). I even wrote down the plan in a notebook, went through it in my head and everything, and I don't understand why I would still need a while loop if I'm using recursion?
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -22,4 +34,5 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
