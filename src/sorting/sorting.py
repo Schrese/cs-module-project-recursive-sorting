839134158cb1fc3003391bc6ae40e74e3f6f7eed
Schrese <schreseholloway@gmail.com>
2020-06-16 17:54:46 -0400

    planning out merge helper function

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,10 +1,24 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
+    # the total length of each array passed in
     elements = len(arrA) + len(arrB)
+    # creating an array with that total length 
     merged_arr = [0] * elements
 
     # Your code here
-
+    # as long as the length of arrA AND arrB is more than 0, I want to keep doing this:
+        # if the value at the front of arrA is less than the value at the front of arrB
+            # save that value
+            # then I want to pop that value off of the array
+            # I want to also push that element onto the holding array (merged_arr)
+        # otherwise I want to do the opposite:
+            # save the value of arrB
+            # pop that value off the array
+            # pusht that element onto the holding array
+        # if the length of arrA is 0
+            # I want to append arrB onto the end of the merged_arr
+        # if the length of arrB is 0
+            # I want to append arrA onto the end of the merged_arr
 
     return merged_arr
 
