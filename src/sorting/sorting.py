d2d877acbc53f23dad168b045a7efe9fa325f7e4
Schrese <schreseholloway@gmail.com>
2020-06-16 18:51:49 -0400

    merge function (sortof) works It's 2x the length it should be.

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -6,21 +6,64 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-    # as long as the length of arrA AND arrB is more than 0, I want to keep doing this:
-        # if the value at the front of arrA is less than the value at the front of arrB
-            # save that value
-            # then I want to pop that value off of the array
-            # I want to also push that element onto the holding array (merged_arr)
-        # otherwise I want to do the opposite:
-            # save the value of arrB
-            # pop that value off the array
-            # pusht that element onto the holding array
-        # if the length of arrA is 0
-            # I want to append arrB onto the end of the merged_arr
-        # if the length of arrB is 0
-            # I want to append arrA onto the end of the merged_arr
+    little_a = 0
+    little_b = 0
+    index_incrementer = 0
+
+    while little_a < len(arrA) and little_b < len(arrB):
+        if arrA[little_a] < arrB[little_b]:
+            merged_arr.insert(index_incrementer, arrA[little_a])
+            little_a += 1
+            index_incrementer += 1
+        else:
+            merged_arr.insert(index_incrementer, arrB[little_b])
+            little_b += 1
+            index_incrementer += 1
+    while little_b < len(arrB):
+        merged_arr.insert(index_incrementer, arrB[little_b])
+        little_b += 1
+        index_incrementer += 1
+    while little_a < len(arrA):
+        merged_arr.insert(index_incrementer, arrA[little_a])
+        little_a += 1
+        index_incrementer += 1
 
     return merged_arr
+
+
+    # I was super close with below
+    # as long as the length of arrA AND arrB is more than 0, I want to keep doing this:
+    # while len(arrA) > 0 and len(arrB) > 0:
+    #     # if the value at the front of arrA is less than the value at the front of arrB
+    #     if arrA[0] < arrB[0]:
+    #         # save that value
+    #         saved = arrA[0]
+    #         # then I want to pop that value off of the array
+    #         arrA.pop(0)
+    #         # I want to also push that element onto the holding array (merged_arr)
+    #         merged_arr.append(saved)
+    #     # otherwise I want to do the opposite:
+    #     if arrB[0] < arrB[0]:
+    #         # save the value of arrB
+    #         saved = arrB[0]
+    #         # pop that value off the array
+    #         arrB.pop(0)
+    #         # pusht that element onto the holding array
+    #         merged_arr.append(saved)
+    #     # if the length of arrA is 0
+    #     if len(arrA) == 0:
+    #         # I want to append arrB onto the end of the merged_arr
+    #         merged_arr.append(arrB)
+    #     # if the length of arrB is 0
+    #     if len(arrB) == 0:
+    #         # I want to append arrA onto the end of the merged_arr
+    #         merged_arr.append(arrA)
+
+    # return merged_arr
+
+a = [1, 4, 7]
+b = [2, 3, 8, 9]
+print(merge(a, b))
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
@@ -35,8 +78,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
